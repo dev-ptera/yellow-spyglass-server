88b7b7affeccfc8d5c319a21a9b1bf8915dfdfde
dev-ptera <dev.ptera@gmail.com>
2021-06-13 18:20:41 -0400

    Add more advanced insights tab

--- src/services/api/account-insights.service.ts
+++ src/services/api/account-insights.service.ts
@@ -15,9 +15,11 @@ const confirmedTransactionsPromise = (address: string): Promise<InsightsDto> =>
             const transactionCount = accountHistory.history.length;
 
             // wtf?
-            const chartIndexIncrement = Math.round(Math.max(1, transactionCount / MAX_CHART_POINTS));
-            let chartIndex = 0;
+            //    const chartIndexIncrement = Math.round(Math.max(1, transactionCount / MAX_CHART_POINTS));
+            //    let chartIndex = 0;
 
+            const accountSentMap = new Map<string, number>();
+            const accountReceivedMap = new Map<string, number>();
             const insightsDto: InsightsDto = {
                 data: [],
                 maxAmountReceivedHash: undefined,
@@ -26,22 +28,35 @@ const confirmedTransactionsPromise = (address: string): Promise<InsightsDto> =>
                 maxAmountSentBan: 0,
                 maxBalanceHash: undefined,
                 maxBalanceBan: 0,
+                mostCommonRecipientAddress: undefined,
+                mostCommonSenderAddress: undefined,
+                mostCommonRecipientTxCount: 0,
+                mostCommonSenderTxCount: 0,
             };
 
             let balance = 0;
             let index = 0;
             for (const transaction of accountHistory.history) {
-                const addPoint = index++ === chartIndex;
+                const isLastDp = index === accountHistory.history.length;
+                // const addPoint = index++ === chartIndex;
+                const addPoint = true;
                 if (transaction.amount) {
                     const ban = Number(Number(rawToBan(transaction.amount)).toFixed(6));
+                    const addr = transaction.account;
                     if (transaction['subtype'] === 'receive') {
                         balance += ban;
+                        accountReceivedMap.has(addr)
+                            ? accountReceivedMap.set(addr, accountReceivedMap.get(addr) + 1)
+                            : accountReceivedMap.set(addr, 1);
                         if (ban > insightsDto.maxAmountReceivedBan) {
                             insightsDto.maxAmountReceivedBan = ban;
                             insightsDto.maxAmountReceivedHash = transaction.hash;
                         }
                     } else if (transaction['subtype'] === 'send') {
                         balance -= ban;
+                        accountSentMap.has(addr)
+                            ? accountSentMap.set(addr, accountSentMap.get(addr) + 1)
+                            : accountSentMap.set(addr, 1);
                         if (ban > insightsDto.maxAmountSentBan) {
                             insightsDto.maxAmountSentBan = ban;
                             insightsDto.maxAmountSentHash = transaction.hash;
@@ -52,11 +67,29 @@ const confirmedTransactionsPromise = (address: string): Promise<InsightsDto> =>
                         insightsDto.maxBalanceHash = transaction.hash;
                     }
                 }
-                if (addPoint) {
-                    chartIndex += chartIndexIncrement;
+                if (addPoint || isLastDp) {
+                    //     chartIndex += chartIndexIncrement;
                     const height = Number(transaction.height);
                     const roundedBalance = balance > 100 ? Math.round(balance) : balance;
-                    insightsDto.data.push({ balance: roundedBalance, height });
+                    insightsDto.data.push({ balance: Number(roundedBalance.toFixed(3)), height });
+                }
+            }
+
+            // Set most common sender/recipient.
+            let accountMaxSentCount = 0; // Calc Recipient Data
+            for (const key of accountSentMap.keys()) {
+                if (accountSentMap.get(key) > accountMaxSentCount) {
+                    accountMaxSentCount = accountSentMap.get(key);
+                    insightsDto.mostCommonRecipientAddress = key;
+                    insightsDto.mostCommonRecipientTxCount = accountMaxSentCount;
+                }
+            }
+            let accountMaxReceivedCount = 0; // Calc Sender/Received Data
+            for (const key of accountReceivedMap.keys()) {
+                if (accountReceivedMap.get(key) > accountMaxReceivedCount) {
+                    accountMaxReceivedCount = accountReceivedMap.get(key);
+                    insightsDto.mostCommonSenderAddress = key;
+                    insightsDto.mostCommonSenderTxCount = accountMaxReceivedCount;
                 }
             }
             return Promise.resolve(insightsDto);
