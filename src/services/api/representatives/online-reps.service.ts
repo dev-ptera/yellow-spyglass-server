fe8666ed854dcb651eb1fa2fbef4cf66ca24b974
dev-ptera <dev.ptera@gmail.com>
2021-07-13 18:54:20 -0400

    Null check

--- src/services/api/representatives/online-reps.service.ts
+++ src/services/api/representatives/online-reps.service.ts
@@ -1,7 +1,6 @@
 import { AppCache, NANO_CLIENT } from '@app/config';
 import * as RPC from '@dev-ptera/nano-node-rpc';
 import { RepresentativeDto } from '@app/types';
-import { on } from 'cluster';
 
 /** Returns the list of online representatives.
  *  Whenever invoked, checks if a previously-offline rep has gone online & updates the AppCache accordingly.
@@ -12,7 +11,7 @@ export const getOnlineReps = async (req, res): Promise<void> => {
     const onlineReps = (await NANO_CLIENT.representatives_online()) as RPC.RepresentativesOnlineResponse;
 
     const offlineTrackedReps = new Map<string, RepresentativeDto>();
-    AppCache.trackedReps.thresholdReps.map((rep) => {
+    AppCache.trackedReps?.thresholdReps.map((rep) => {
         rep.online ? online.add(rep.address) : offlineTrackedReps.set(rep.address, rep);
     });
 
