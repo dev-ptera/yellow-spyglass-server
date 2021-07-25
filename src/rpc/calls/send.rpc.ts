b092d2d7d0a3b511ea4bc62560e4cf4acea2ab24
dev-ptera <dev.ptera@gmail.com>
2021-07-25 07:32:31 -0400

    Megaphone pickasmallrepboi

--- src/rpc/calls/send.rpc.ts
+++ src/rpc/calls/send.rpc.ts
@@ -1,12 +1,12 @@
 import { NANO_CLIENT } from '@app/config';
 
-export const sendRpc = async (wallet: string, source: string, destination: string): Promise<any> =>
+export const sendRpc = async (wallet: string, source: string, destination: string, amount: string): Promise<any> =>
     // @ts-ignore
     NANO_CLIENT._send('send', {
         wallet: wallet,
         source: source,
         destination: destination,
-        amount: '1000000000000000000000000000',
+        amount
     })
         .then((response: any) => Promise.resolve(response))
         .catch((err) => Promise.reject(err));
--- src/services/api/megaphone/megaphone.service.ts
+++ src/services/api/megaphone/megaphone.service.ts
@@ -2,10 +2,16 @@ import { sendRpc } from '@app/rpc';
 import { LOG_ERR, LOG_INFO } from '@app/services';
 import { megaphoneAccounts } from '../../../config/megaphone-accounts';
 
-const sendFunds = (wallet: string, source: string, destination: string): Promise<any> =>
+/** Wait interval in milliseconds. */
+export const sleep = (ms) =>
+    new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+
+const sendFunds = (wallet: string, source: string, destination: string, amount: string): Promise<any> =>
     new Promise((resolve) => {
         const start = LOG_INFO(`Sending BAN to account ${destination}`);
-        sendRpc(wallet, source, destination)
+        sendRpc(wallet, source, destination, amount)
             .then(() => {
                 LOG_INFO('Sent funds successfully', start);
                 resolve();
@@ -18,20 +24,27 @@ const sendFunds = (wallet: string, source: string, destination: string): Promise
 
 /** Sends selected accounts messages. */
 export const useMegaphone = async (req, res) => {
-    const hasLargeRep: string[] = req.body.hasLargeRep;
-    const hasOfflineRep: string[] = req.body.hasOfflineRep;
+    const hasLargeRep: string[] = req.body.hasLargeRep || [];
+    const hasOfflineRep: string[] = req.body.hasOfflineRep || [];
 
     console.log(hasLargeRep);
     console.log(hasOfflineRep);
 
-    const largeRep = megaphoneAccounts.hasLargeRepMessage;
+    const picka = megaphoneAccounts.picka;
+    const small = megaphoneAccounts.small;
+    const repboi = megaphoneAccounts.repboi;
+
     for (const address of hasLargeRep) {
-        await sendFunds(largeRep.wallet, largeRep.address, address);
+        await sendFunds(picka.wallet, picka.address, address, '1000000000000000000000000000');
+        await sleep(2000);
+        await sendFunds(small.wallet, small.address, address, '2000000000000000000000000000');
+        await sleep(2000);
+        await sendFunds(repboi.wallet, repboi.address, address, '3000000000000000000000000000');
     }
 
     const offlineRep = megaphoneAccounts.hasOfflineRepMessage;
     for (const address of hasOfflineRep) {
-        await sendFunds(offlineRep.wallet, offlineRep.address, address);
+        await sendFunds(offlineRep.wallet, offlineRep.address, address, '1000000000000000000000000000');
     }
 
     res.status(200).send(JSON.stringify({ success: true }));
