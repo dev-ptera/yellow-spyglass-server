b8e46527cdb0fdabe9330b04eb34e6cbbd13e423
dev-ptera <dev.ptera@gmail.com>
2021-08-14 09:23:04 -0400

    Track ledger size

--- src/services/api/node/node-stats.service.ts
+++ src/services/api/node/node-stats.service.ts
@@ -1,14 +1,21 @@
 import { ErrorResponse } from '@dev-ptera/nano-node-rpc';
 import { LOG_ERR } from '@app/services';
 import { AppCache } from '@app/config';
+const getSize = require('get-folder-size');
 
 /** Returns statistics of this explorer's node. */
 export const getNodeStats = (req, res): void => {
     if (AppCache.representatives) {
         for (const dto of AppCache.representatives.monitoredReps) {
             if (dto.name === 'batman') {
-                return res.send({
-                    ...dto,
+                const myFolder = 'C:\\Windows\\Containers\\serviced';
+                getSize(myFolder, (err, size) => {
+                    if (err) { throw err; }
+                    const ledgerSizeMb = (size / 1024 / 1024).toFixed(2);
+                    return res.send({
+                        ...dto,
+                        ledgerSizeMb
+                    });
                 });
             }
         }
