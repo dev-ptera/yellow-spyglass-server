d8c2b77ec01cd3931ff87778a2848a7eb07caf62
dev-ptera <dev.ptera@gmail.com>
2021-10-18 14:39:36 -0400

    Add owner to Jungle TV Prizes

--- src/services/api/known-accounts/manual-accounts.ts
+++ src/services/api/known-accounts/manual-accounts.ts
@@ -388,5 +388,6 @@ export const MANUAL_ACCOUNTS: KnownAccountDto[] = [
         address: 'ban_1jtprixunfus5mozkzj5gtfm79b54p3pwnje8snh9ugu998cfk13qceepwn5',
         alias: "JungleTV Prize Distribution",
         type: 'Distribution',
+        owner: 'gbl08ma',
     },
 ];
